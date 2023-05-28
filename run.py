3849dc674f79bfc61968388199d854832000b8f3
Jesper Dahlberg <jesperdahlberg.jd@gmail.com>
2023-05-28 14:08:21 +0000

    clean up som blank spaces

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-import math  
-import random  
+import math
+import random
 import pyfiglet
 from colorama import Fore, Back, Style
 
-# Declair some variables 
-top_row=Fore.BLUE+'1_______2_______3_______4_______5_______6'+Style.RESET_ALL
-BOMB= Back.RED+"X"+Style.RESET_ALL
+# Declair some variables
+top_row = Fore.BLUE + '1_______2_______3_______4_______5_______6' + Style.RESET_ALL
+BOMB = Back.RED + "X" + Style.RESET_ALL
 result = pyfiglet.figlet_format("Mine Sweeper")
 score = 0
 
@@ -16,22 +16,22 @@ def minesweeper_heading():
     Let the user start the game or read the instructions.
     If the user inputs the wrong inputs the function raises an Error.
     """
-    print(result) #Says MineSweeper.
+    print(result) # Says MineSweeper.
     while True:
         try:
             start_instructions= input("Press 1 to start game\nPress 2 for instructions:")
             if start_instructions== "1":
-                print(top_row) #Starts the game
+                print(top_row) # Starts the game
                 break
-            if start_instructions=="2": #Displayes the instuctions and then continue the loop
+            if start_instructions=="2": # Displayes the instuctions and then continue the loop
                 print("Minesweeper is a game where mines are hidden in a grid of squares.\n"
                 "Safe squares have numbers telling you how many mines touch the square.\n"
                 "You can use the number clues to solve the game by opening all of the safe squares.\n"
                 "If you click on a mine you lose the game!")
-                continue 
-        except NameError as e: #Ops something wrong
-            print(Back.RED+f"You must enter '1' or '2'. You entered:{e}"+Style.RESET_ALL)
-        else: print(Back.RED+f"You must enter '1' or '2'. You entered:{start_instructions}"+Style.RESET_ALL)
+                continue
+        except NameError as e: # Ops something wrong
+            print(Back.RED + f"You must enter '1' or '2'. You entered:{e}" + Style.RESET_ALL)
+        else: print(Back.RED + f"You must enter '1' or '2'. You entered:{start_instructions}" + Style.RESET_ALL)
     return False
 
 
@@ -52,18 +52,18 @@ def minesweeper(n):
         [0,0,0,0,0,0],
     ]
     
-    for num in range(6): #Generates "X" AKA Bombs
+    for num in range(6): # Generates "X" AKA Bombs
         
         x = random.randint(0,n-1)
         y = random.randint(0,n-1)
         arr[y][x] = BOMB
         
         
-        #Logic for the offsets.
-        if (x >=0 and x <= n-2) and (y >= 0 and y <= n-1):
+        # Logic for the offsets.
+        if (x >= 0 and x <= n-2) and (y >= 0 and y <= n-1):
             if arr[y][x+1] != BOMB:
                 arr[y][x+1] += 1 # center right
-        if (x >=1 and x <= n-1) and (y >= 0 and y <= n-1):
+        if (x >= 1 and x <= n-1) and (y >= 0 and y <= n-1):
             if arr[y][x-1] != BOMB:
                 arr[y][x-1] += 1 # center left
         if (x >= 1 and x <= n-1) and (y >= 1 and y <= n-1):
@@ -77,7 +77,7 @@ def minesweeper(n):
             if arr[y-1][x] != BOMB:
                 arr[y-1][x] += 1 # top center
 
-        if (x >=0 and x <= n-2) and (y >= 0 and y <= n-2):
+        if (x >= 0 and x <= n-2) and (y >= 0 and y <= n-2):
             if arr[y+1][x+1] != BOMB:
                 arr[y+1][x+1] += 1 # bottom right
         if (x >= 1 and x <= n-1) and (y >= 0 and y <= n-2):
@@ -117,11 +117,11 @@ def reviel_board(map):
      https://medium.com/swlh/this-is-how-to-create-a-simple-minesweeper-game-in-python-af02077a8de
     """
 
-    for row in map:    
+    for row in map:
         print("\t".join(str(cell) for cell in row))
         print("------------------------------------------")
 
-                
+
 
 def player_choise():
     """
@@ -131,52 +131,52 @@ def player_choise():
     hidden_board=minesweeper(6)
     player_check=player_board()
     while True:
-        global score #scoreboard
+        global score # scoreboard
         
         try:
-            reviel_board(player_check) #The player inputs thier guess
-            print(Back.BLUE+"Enter your cell you want to open: "+Style.RESET_ALL)
-            player_x= input("X: Enter numbers 1-6: ")
-            player_Y= input("Y: Enter numbers 1-6: ")
-            player_x=int(player_x) -1 #input "1"=0
-            player_Y=int(player_Y) -1
+            reviel_board(player_check) # The player inputs thier guess
+            print(Back.BLUE + "Enter your cell you want to open: " + Style.RESET_ALL)
+            player_x = input("X: Enter numbers 1-6: ")
+            player_Y = input("Y: Enter numbers 1-6: ")
+            player_x = int(player_x) -1 # input "1"=0
+            player_Y = int(player_Y) -1
     
-        except ValueError:#Oops!
-            print(Back.RED+"Please enter a number between 1-6."+Style.RESET_ALL)
+        except ValueError:# Oops!
+            print(Back.RED + "Please enter a number between 1-6." + Style.RESET_ALL)
             print(top_row)
             continue
             
-        if player_x <=5 and player_Y <=5:
-            x=player_x #Assign player input to var "X" or "Y" to be checked below
-            y=player_Y
-            #Checks if hiddenboard and playercheck is equal, if it  is an error is raised
+        if player_x <= 5 and player_Y <= 5:
+            x = player_x # Assign player input to var "X" or "Y" to be checked below
+            y = player_Y
+            # Checks if hiddenboard and playercheck is equal, if it  is an error is raised
             if hidden_board[x][y] == player_check[x][y]:
                 print(f"Score: {score}") #Dislpays scoreboard
-                print(Back.RED+"These coordinates have already been used, try again!"+Style.RESET_ALL)
+                print(Back.RED + "These coordinates have already been used, try again!" + Style.RESET_ALL)
                 print(top_row)
                 continue
 
-                #Checks if the user dug a bomb or not
+                # Checks if the user dug a bomb or not
             if hidden_board[x][y]!=BOMB:
                 player_check[x][y] = hidden_board[x][y]
-                score += 1 #You got a point!
+                score += 1 # You got a point!
                 
 
-                #The user dug a bomb
-            if hidden_board[x][y]==BOMB:
+                # The user dug a bomb
+            if hidden_board[x][y] == BOMB:
                 print(top_row)
-                reviel_board(hidden_board) #Reviels the board
-                print(Back.RED+"Game over!"+Style.RESET_ALL) #ohno!
-                restart() 
-                
-                #Checks if the user has reached the score of 28
-            if score == 28:
-                print(Back.GREEN+"You won!!"+Style.RESET_ALL)
-                print(f"Score: {score}")
-                restart()#Calls the restart function
-                       
+                reviel_board(hidden_board) # Reviels the board
+                print(Back.RED + "Game over!" + Style.RESET_ALL) # ohno!
+                restart()
 
-        else: print(Back.RED+f"Please enter a number between 1-6. You entered:{player_x +1} and {player_Y +1}."+Style.RESET_ALL)
+                # Checks if the user has reached the score of 28
+            if score == 28:
+                print(Back.GREEN + "You won!!" + Style.RESET_ALL)
+                print(f"Score: {score}")
+                restart()# Calls the restart function
+
+
+        else: print(Back.RED + f"Please enter a number between 1-6. You entered:{player_x + 1} and {player_Y + 1}." + Style.RESET_ALL)
         print(top_row)
         
         
@@ -188,29 +188,29 @@ def restart():
     Allows the user to restart the game
     """
     while True:
-        restart_game = input("Restart? Y/N:") #Yes or no?
+        restart_game = input("Restart? Y/N:") # Yes or no?
 
-           #if yes                 
+           # if yes
         if restart_game == "y":
             global score
-            score=0 #Score is set to 0
+            score=0 # Score is set to 0
             main() # main function is called
 
-            #if no
+            # if no
         if restart_game == "n":
             print("Exit program.")
-            exit() #Exit program
+            exit() # Exit program
 
-        #Error message
+        # Error message
         else: print(f"Please answer Yes or No, You entered:{restart_game}")
-    return False    
+    return False
 
-     
+
 def main():
     """
     Runs all functions.
     """
-    score=0
+    score = 0
     minesweeper_heading()
     minesweeper(6)
     player_board()
